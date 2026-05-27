5f8341026d7138d5562ad1128c49a54d08328627
Flafiee <whitenremi@gmail.com>
2026-05-27 08:33:00 +0300

    index.php päivitetty kopiotu vanhasta tehtävästä base koodi, tullaan muuttamaan, haluan vain nähdä, että toimiiko

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>Let's kirjaudu</h1>
+    <form action="login.php" method="post">
+        <label for="username">Käyttäjä</label>
+        <input type="text" name="username" id="usernamex"><br>
+        <label for="password">Passu</label>
+        <input type="password" name="password" id="passwordx"><br>
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
